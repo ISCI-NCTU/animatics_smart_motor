16abba9ca85893831312e537239ef33cdd8f3a97
gcheval <chevalier321@gmail.com>
2014-06-08 18:57:16 -0400

    .

--- scripts/smartmotorcalibrator/smartmotorcalibrator.py
+++ scripts/smartmotorcalibrator/smartmotorcalibrator.py
@@ -1,26 +1,25 @@
 # ! /usr/bin/env python
+
+__author__ = 'guillaumechevalier'
+
 import time
 import math
 import numpy
 import tf
+import roslib
+import rospy
 from capra_msgs.srv import *
-from twisted.python.formmethod import Integer
 from geometry_msgs.msg import Twist
 from thread import start_new_thread
 
-import roslib
 from sensor_msgs.msg import Imu
 
 roslib.load_manifest('capra_motors')
-import rospy
-
-__author__ = 'guillaumechevalier'
 
 VERIFICATION_TIME = 3
 CONFIGURATION_TIME = 3
 WAIT_FOR_SERVICE_TIMEOUT = 5
 ORIENTATION_THRESHOLD = 1.0
-
 
 class SmartmotorCalibrator:
     def __init__(self):
@@ -47,11 +46,8 @@ class SmartmotorCalibrator:
         if not self.imu_functional:
             o = msg.orientation
             r, p, y = tf.transformations.euler_from_quaternion([o.x, o.y, o.z, o.w])
-            theta = y
-            if (theta > 0):
-                self.currentRotation = numpy.matrix([[math.cos(theta), -math.sin(theta)], [math.sin(theta), math.cos(theta)]])
-            else:
-                self.currentRotation = numpy.matrix([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])
+            self.currentRotation = y * 180 / math.pi
+
             print "rotation = " + self.rotation
             if (self.initial_imu_orientation == None):
                 self.initial_imu_orientation = self.currentRotation
